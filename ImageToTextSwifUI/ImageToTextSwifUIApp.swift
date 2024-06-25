c085a712d9b6932ccd175bab2fa1f46c61f18cd5
Krystynka86 <71220344+Krystynka86@users.noreply.github.com>
2024-06-25 09:37:36 +0200

    Initial Commit

---
+++ ImageToTextSwifUI/ImageToTextSwifUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageToTextSwifUIApp.swift
+//  ImageToTextSwifUI
+//
+//  Created by Cristina on 2024-06-25.
+//
+
+import SwiftUI
+
+@main
+struct ImageToTextSwifUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
